9420d8c7c85321bbc9226a91e2b477981b342a4d
Ben Richmond <ben_richmond@ntlworld.com>
2023-01-15 09:39:23 +0000

    flashcard app completed

--- Day_31_Flashcard_App/main.py
+++ Day_31_Flashcard_App/main.py
@@ -1,4 +1,4 @@
-#from ui import *
+#import libraries
 import random
 from tkinter import *
 import pandas as pd
@@ -6,22 +6,33 @@ import pandas as pd
 MAIN_FILEPATH = "Day_31_Flashcard_App/"
 BACKGROUND_COLOR = "#B1DDC6"
 
+#Possible functionality to add
+## Add a reset button to start again by deleting the to_learn file
+## Add way of selecting flashcards to use from a set
+## on start up, say how long you want before it flips
+
 #read in csv data, and convert to each line to a dictionary
-words_fr_en = pd.read_csv(MAIN_FILEPATH+"data/french_words.csv")
+try:
+    words_fr_en = pd.read_csv(MAIN_FILEPATH+"data/words_to_learn_fr_en.csv")
+except FileNotFoundError:
+    words_fr_en = pd.read_csv(MAIN_FILEPATH+"data/french_words.csv")
+    words_fr_en.to_csv(MAIN_FILEPATH+"data/words_to_learn_fr_en.csv")
+
 dict_fr_en= words_fr_en.to_dict(orient="records")
 current_card = {}
 
 
 
-
 def next_card():
-    #choose a random word from the dictionary, and show it on the card
     global current_card, flip_timer
+    #when a new card is selected, reset the flip timer
     window.after_cancel(flip_timer)
+    #choose a random word from the dictionary, and show it on the card
     current_card = random.choice(dict_fr_en)
     canvas.itemconfig(card_background, image = canvas_img_front)
     canvas.itemconfig(card_title, text = "French", fill="black")
     canvas.itemconfig(card_word, text = current_card["French"], fill="black")
+    #reinstate the flip timer
     flip_timer = window.after(3000, flip_card)
     
 
@@ -34,6 +45,13 @@ def flip_card():
     canvas.itemconfig(card_background, image = canvas_img_back)
     next
 
+#if the meaning is known, then remove the word from the list
+def is_known():
+    dict_fr_en.remove(current_card)
+    new_to_learn = pd.DataFrame(dict_fr_en)
+    new_to_learn.to_csv(MAIN_FILEPATH+"data/words_to_learn_fr_en.csv", index=False)
+    next_card()
+    
 
 
 window = Tk()
@@ -59,7 +77,7 @@ button_wrong = Button(image=img_wrong, highlightthickness=0, command=next_card)
 button_wrong.grid(column=0,row=1)
 
 img_correct = PhotoImage(file = MAIN_FILEPATH+"images/right.png")
-button_correct = Button(image=img_correct, highlightthickness=0, command=next_card)
+button_correct = Button(image=img_correct, highlightthickness=0, command=is_known)
 button_correct.grid(column=1,row=1)
 
 next_card()
